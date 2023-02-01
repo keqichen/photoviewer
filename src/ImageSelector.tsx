493adf419e64025570ac2f1072d351614839077c
Joe Fry <103355865+OKPersonalComputer@users.noreply.github.com>
2023-02-01 15:28:36 +0000

    Create thumbnail image display

---
+++ src/ImageSelector.tsx
@@ -0,0 +1,28 @@
+import React from "react";  // import React (to provide access to TSX)
+import './ImageSelector.css'
+
+export function ImageSelector() {
+    const urls = [];
+    const brokenImages = [
+        1, 24, 32, 36, 44, 47
+    ];
+
+    for (let i = 0; i < 50; i++) {
+        if (!brokenImages.includes(i)){
+            const imageNumberString = i.toString().padStart(2, '0');
+            urls.push(`https://picsum.photos/id/6${imageNumberString}/1600/1600.jpg`)
+        }
+    }
+
+
+    const jsxElement = [];
+    for (let i = 0; i < urls.length; i++) {
+        jsxElement.push(<img className="thumbnail-image" src={urls[i]}></img>)
+    }
+
+    return (<div className="selector-image">
+        {jsxElement}
+    </div>)
+
+
+}
