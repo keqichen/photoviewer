e92568e30ec346bb128b59b8c624620e87492849
Joe Fry <103355865+OKPersonalComputer@users.noreply.github.com>
2023-02-02 11:46:27 +0000

    Add photo selection functionality

--- src/ImageSelector.tsx
+++ src/ImageSelector.tsx
@@ -1,4 +1,5 @@
 import React from "react";  // import React (to provide access to TSX)
+
 import './ImageSelector.css'
 
 export function ImageSelector() {
@@ -8,24 +9,48 @@ export function ImageSelector() {
     ];
 
     for (let i = 0; i < 50; i++) {
-        if (!brokenImages.includes(i)){
+        if (!brokenImages.includes(i)) {
             const imageNumberString = i.toString().padStart(2, '0');
             urls.push(`https://picsum.photos/id/6${imageNumberString}/1600/1600.jpg`)
         }
     }
 
+    const [image, setImage] = React.useState("");
+
     const jsxElement = [];
     for (let i = 0; i < urls.length; i++) {
-        jsxElement.push(<img className="thumbnail-image" src={urls[i]}></img>)
+        const newUrl = urls[i];
+        jsxElement.push(
+            <img className="thumbnail-image"
+                src={urls[i]}
+                onClick={function () {
+                    setImage(newUrl);
+                }}>
+            </img>)
     }
 
+    // update the img id in the jsxElement array;
+    for (let i = 0; i < jsxElement.length; i++) {
+        if (urls[i] === image) {
+            jsxElement[i] = <img className="thumbnail-image"
+                id="thumbnail-image-selected" src={urls[i]}></img>
+        }
+    }
+
+
+
     return (
-    <>
-    <h2>Select your photo</h2>
-    <div className="selector-image">
-        {jsxElement}
-    </div>
-    </>
+        <>
+            <div>{image}</div>
+            <h2>Select your photo</h2>
+            <div className="selector-image">
+                {jsxElement}
+            </div>
+        </>
     )
 
+
+
 }
+
+
--- src/photoviewer/PhotoViewer.tsx
+++ src/photoviewer/PhotoViewer.tsx
@@ -1,11 +1,16 @@
 import React from "react";  // import React (to provide access to TSX)
 import './PhotoViewer.css'
 
-export function PhotoViewer(props: {imgUrl: string}) {    // declare and export new function called 'PhotoViewer'
+export function PhotoViewer(props: PhotoUrl) {    // declare and export new function called 'PhotoViewer'
 
     return (
         <div className="image-wrapper">
             <img className="image" src={props.imgUrl} alt="this is a image"></img>
+
         </div>
     );
+}
+
+interface PhotoUrl {
+    imgUrl: string,
 }
