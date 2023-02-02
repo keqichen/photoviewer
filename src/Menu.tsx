3ea8644f1feff37f99a6a2dd50a87522e069a4bc
Keqi Chen <ckq009@gmail.com>
2023-02-02 15:54:08 +0000

    Implement a toggle menu button with useState

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -1,17 +1,20 @@
 import React from "react";  // import React (to provide access to TSX)
+import * as ReactDOM from 'react-dom';
 import "./Menu.css"
 
 
 export function Menu() {
+    const [menu,setMenu] = React.useState(false);
+
     return (
         <>
-            <nav className="menu-wrapper">
+            <nav className={menu?"menu-appear":"menu-wrapper"}>
                 <ul className="menu">
                     <li className="menu-items">Home</li>
                     <li className="menu-items">Photos</li>
                 </ul> 
             </nav>
-            <button className="button" onClick={() => console.log("hi")}>Menu</button>
+            <button className="button" onClick={()=>setMenu(!menu)}>Menu</button>
         </>
 
 
