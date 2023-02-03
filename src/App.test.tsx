e5da79df261f51527951bf56d43b315fe7c6e71a
Keqi Chen <ckq009@gmail.com>
2023-02-03 16:06:52 +0000

    Complete component test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,7 +5,7 @@ import { PhotoViewer } from './photoviewer/PhotoViewer';
 import { render, screen, fireEvent } from '@testing-library/react';
 import * as renderer from 'react-test-renderer';
 
-/*
+
 test('renders React text', () => {
     const { getByText } = render(<App />);
     const textElement = getByText(/React/i);
@@ -23,58 +23,30 @@ test('renders and checks PhotoViewer', () => {
     expect(image).toHaveAttribute('src', 'https://picsum.photos/id/600/600/600.jpg')
 });
 
-
 test('Check first image equal to what we are expecting', () => {
     render(<App />);
     const image = screen.getAllByRole('img')[0];
     expect(image).toHaveAttribute('src', 'https://picsum.photos/id/600/600/600.jpg')
 });
 
-
 //A ‘Regression / Snapshot' test 
 //to confirm that I the ‘ImageViewer' component 
 //doesn’t accidentally change in the future.
-
 
 it('renders correctly', async () => {
     const tree = renderer.create(<PhotoViewer imgUrl={'https://picsum.photos/id/600/600/600.jpg'} />).toJSON();
     expect(tree).toMatchSnapshot();
 });
 
-
-/* this snapshot test is now failing
 it('renders correctly2', async () => {
     const tree = renderer.create(<App />).toJSON();
     expect(tree).toMatchSnapshot();
 });
-*/
-
-/*
-describe("My Component", () => {    
-    it("Should match snapshot without name prop", async () => {       
-    const tree = renderer.create(<MyComponent/>).toJSON();                
-    expect(tree).toMatchSnapshot();    });
-*/
 
 // A Component Test 
 // to confirm that when I click a thumbnail
 // then the thumbnail becomes selected 
 // and the ImageViewer updates with the new image.
-
-
-/*
-describe("My Component", () => {
-    it("should become selected", async () => {
-        const component = render(<ImageSelector setState={function dispatchSetState() { }} image="https://picsum.photos/id/600/600/600.jpg" />);
-        await wait(() => component.getByText("Hello World!"));
-    });
-
-    it("should update the image in the PhotoViewer", async () => {
-        const component = render(<App/>);
-        await wait(() => component.getByText("React Photo Viewer"));
-    });
-});
-*/
 
 describe("My Component", () => {
     it("Should check the image has been selected", async () => {
@@ -91,5 +63,3 @@ describe("My Component", () => {
 });
 
 
-// React.Dispatch<React.SetStateAction<"https://picsum.photos/id/602/600/600.jpg">>
-// React.Dispatch<React.SetStateAction<"https://picsum.photos/id/602/600/600.jpg">>
